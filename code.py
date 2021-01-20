7458ecd1638a6f0672677824ac05332dd01fc0c5
Umang Rajkarnikar <rajkarnu@mcmaster.ca>
2021-01-20 17:06:59 -0500

    Upload text file.

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ def are_valid_groups(students, groups):
 
     return result
 
-print(are_valid_groups([1,2,3],[[1,2,3],[4,5,6,7,8]]))
+print(are_valid_groups(["1","2","3"],[["1","2","3"],["4","5","6","7","8"]]))
 
 
 
